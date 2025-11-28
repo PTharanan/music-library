7161f3f8e9e8eece81b1fee5a3ca5c9721ae2503
ASUS <ptharanan@gmail.com>
2025-11-28 17:24:23 +0530

    Deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-z5@*yp(_t3wezzg8d@+2jiq)^1q3q1q2x&-yyp+qkzk3nx%a&8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '*' ]
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+]
 
 
 
@@ -46,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -55,12 +58,6 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:4200",  # Angular
-    "http://127.0.0.1:4200",
-]
-
 
 ROOT_URLCONF = 'backend.urls'
 
@@ -128,6 +125,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
