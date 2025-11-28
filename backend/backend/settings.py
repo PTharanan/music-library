ad94b4e8802462f92d702e2a814dabad5b114862
ASUS <ptharanan@gmail.com>
2025-11-28 18:17:40 +0530

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,13 +128,6 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
