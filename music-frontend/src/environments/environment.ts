dc0a919224345ad71c4ac6dad53e32c1b58e7de1
Tharanan <ptharanan@gmail.com>
2025-12-24 00:31:27 +0530

    Fix syntax error in environment.ts

--- music-frontend/src/environments/environment.ts
+++ music-frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // apiUrl: 'http://127.0.0.1:8000/api/music/'
   
- apiUrl = 'https://tharanan.pythonanywhere.com//api/music/'
+  apiUrl: 'https://tharanan.pythonanywhere.com//api/music/'
 };
